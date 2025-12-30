9b27ea26d79247c3a38b029337f4445cb5452308
telegramamsecond <104004811+telegramamsecond@users.noreply.github.com>
2025-12-30 10:28:19 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -38,7 +38,7 @@ async def save_file(media):
 
     # TODO: Find better way to get same file_id for same media to avoid duplicates
     file_id, file_ref = unpack_new_file_id(media.file_id)
-    file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
+    file_name = re.sub(r"(_|\-|\.|\+|@LinK_YoGi_LoVer)", " ", str(media.file_name))
     try:
         file = Media(
             file_id=file_id,
